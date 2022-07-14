2f635e1cca15ba1e9d7c0c2c9fc05b223f916fd3
danielefongo <danielefongo@gmail.com>
2022-07-14 16:41:37 +0200

    add docker

--- install.sh
+++ install.sh
@@ -34,3 +34,18 @@ asdf plugin add nodejs
 asdf plugin add python
 asdf plugin add rust
 asdf install
+
+# docker
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+if ![[ -f /etc/apt/keyrings/docker.gpg ]]; then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+fi
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+if [[ -z "$(groups | grep docker)" ]] ; then
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+fi
