c51d309a9f5b9bb7d16df2d8fb900847c465d999
danielefongo <danielefongo@gmail.com>
2022-07-14 14:26:48 +0200

    add some terminal utils

---
+++ install.sh
@@ -0,0 +1,17 @@
+# terminal
+sudo apt-get install hub -y
+sudo apt-get install tig -y
+sudo apt-get install delta -y
+sudo apt-get install autojump -y
+sudo apt-get install ripgrep -y
+sudo apt-get install tree -y
+sudo apt-get install bat -y
+sudo apt-get install tldr -y
+sudo apt-get install jq -y
+sudo apt-get install ncdu -y
+sudo apt-get install pidof -y
+sudo apt-get install htop -y
+sudo apt-get install httpie -y
+sudo apt-get install fzf -y
+sudo apt-get install gpg -y
+sudo apt-get install cloc -y
