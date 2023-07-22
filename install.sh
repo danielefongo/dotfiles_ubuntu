4e0be400d18daaf4efba8a86577d28e32b8a8eef
danielefongo <danielefongo@gmail.com>
2023-07-22 11:50:35 +0200

    update dots

--- install.sh
+++ install.sh
@@ -34,31 +34,22 @@ sudo apt-get install alacritty -y
 sudo apt-get install feh -y
 sudo apt-get install picom -y
 
-# asdf
+# asdf deps
 sudo apt-get install build-essential automake autoconf libncurses5-dev libssl-dev libffi-dev zlib1g-dev -y
-
-source dots/asdf/asdf.sh
-cat ~/.tool-versions | cut -f1 -d' ' | grep -ve '^$' | while read package; do
-  asdf plugin add $package
-done
-asdf install
 
 # docker
 sudo apt-get install ca-certificates curl gnupg lsb-release -y
 sudo mkdir -p /etc/apt/keyrings
 if ! [[ -f /etc/apt/keyrings/docker.gpg ]]; then
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 fi
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-if [[ -z "$(groups | grep docker)" ]] ; then
-  sudo groupadd docker
-  sudo usermod -aG docker $USER
+if [[ -z "$(groups | grep docker)" ]]; then
+	sudo groupadd docker
+	sudo usermod -aG docker $USER
 fi
 
-(
-  cd dots
-  ./setup.sh
-)
+./dots/scripts/dots_setup
